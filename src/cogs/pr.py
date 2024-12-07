6e01addcfb9d904b444f8f708dd9eae6ae48a17b
Pingy5600 <Jeromedemaertelaere@gmail.com>
2024-12-07 17:27:18 +0100

    bug fix, colors, errors

--- src/cogs/pr.py
+++ src/cogs/pr.py
@@ -5,12 +5,12 @@ from discord.ext import commands
 from datetime import datetime
 from database import db_manager
 from embeds import DefaultEmbed
+from embeds import OperationFailedEmbed
 
 
 class pr(commands.Cog, name="pr"):
     def __init__(self,bot):
         self.bot = bot
-
 
     @discord.app_commands.command(name="work", description="checks to see if I am online")
     async def work(self, interaction: discord.Interaction):
@@ -21,9 +21,20 @@ class pr(commands.Cog, name="pr"):
         await interaction.response.send_message(embed=embed)
 
 
+    @discord.app_commands.command(name="info", description="Provides information about the bot")
+    async def info(self, interaction: discord.Interaction):
+        embed = DefaultEmbed(
+            title="Bot Info",
+            description="This bot was created to track PRs and help with fitness goals! 🏋️‍♂️"
+        )
+        embed.add_field(name="Version", value="1.0.0", inline=True)
+        embed.add_field(name="Developer", value="Pingy1", inline=True)
+        await interaction.response.send_message(embed=embed)
+
+
     @discord.app_commands.command(name = "pr", description = "adds pr to the user's name")
     @discord.app_commands.describe(date="The date of the pr", pr="The personal record value", user="Which user")
-    async def add_pr(self, interaction: discord.Interaction, pr: int, date: str = None, user: discord.User = None):
+    async def add_pr(self, interaction: discord.Interaction, pr: str, date: str = None, user: discord.User = None):
         await interaction.response.defer(thinking=True)
 
         if user is None:
@@ -32,6 +43,21 @@ class pr(commands.Cog, name="pr"):
         if date is None:
             date = 'vandaag'
 
+        pr_cleaned = pr.replace(',', '.')
+        pr_command_ref = f"</pr:{self.bot.tree.get_command('pr').id}>"
+
+        try:
+            pr = float(pr_cleaned)
+
+        except ValueError:
+
+            embed = OperationFailedEmbed(
+                description=
+                "You provided an invalid PR value. Please use the correct format.\n"
+                f"Please try again: {pr_command_ref}"
+            )
+            return await interaction.followup.send(embed=embed)
+        
         try:
             date_obj = dateparser.parse(date, settings={
                 'DATE_ORDER': 'DMY',
@@ -45,23 +71,35 @@ class pr(commands.Cog, name="pr"):
                 raise ValueError("Invalid date format")
 
         except ValueError:
-            return await interaction.followup.send(
-                "Ongeldige datum. Gebruik een formaat zoals '2023-11-25' of '25 november 2023'."
+            embed = OperationFailedEmbed(
+                description=
+                "Invalid date. Use a format like '2024-11-25' or 'November 25, 2024'.\n"
+                f"Please try again: {pr_command_ref}"
             )
+            return await interaction.followup.send(embed=embed)
+
         except Exception as e:
-            return await interaction.followup.send(
-                f"Er is een fout opgetreden: {e}"
+            embed = OperationFailedEmbed(
+                description=f"An error has occurred:{e}"
             )
+            return await interaction.followup.send(embed=embed)
 
         resultaat = await db_manager.add_pr(user.id, "bench", pr, date_obj)
-        if resultaat[0]:
-            return await interaction.followup.send(
-                f"PR van {pr}kg op {date_obj.strftime('%d/%m/%y')} is toegevoegd!"
-            )
 
-        await interaction.followup.send(
-            f"Er is iets misgegaan: {resultaat[1]}"
+        if resultaat[0]:
+            embed = DefaultEmbed(
+                title="PR added!",
+                description=f"PR of {pr}kg added"
+            )
+            embed.add_field(name="User", value=user.mention, inline=True)
+            embed.add_field(name="Excercise", value="bench", inline=True)
+            embed.add_field(name="Date", value=date_obj.strftime('%d/%m/%y'), inline=True)
+            return await interaction.followup.send(embed=embed)
+        
+        embed = OperationFailedEmbed(
+            description= f"Something went wrong: {resultaat[1]}"
         )
+        await interaction.followup.send(embed=embed)
 
 async def setup(bot):
     await bot.add_cog(pr(bot))
--- src/embeds/__init__.py
+++ src/embeds/__init__.py
@@ -2,16 +2,24 @@ import discord
 from datetime import datetime
 
 
-DEFAULT_COLOR = 0xF4900D
+DEFAULT_COLOR = 0x4169E1
 ERROR_COLOR = 0xE02B2B
 SUCCES_COLOR = 0x39AC39
 
 class DefaultEmbed(discord.Embed):
-
     def __init__(self, title, description=None):
         super().__init__(
             title=f"{title}",
             description=description,
-            # color=DEFAULT_COLOR,
+            color=DEFAULT_COLOR,
             timestamp=datetime.now(),
+        )
+
+class OperationFailedEmbed(discord.Embed):
+    def __init__(self, title="Error", description=None, emoji="❌"):
+        super().__init__(
+            title=f"{title} {emoji}", 
+            description=description,
+            color=ERROR_COLOR,
+            timestamp=datetime.now()
         )
