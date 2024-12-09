ba807f1ab0d30c1717717aca1bf0406b81f99d95
Pingy5600 <Jeromedemaertelaere@gmail.com>
2024-12-09 14:00:45 +0100

    db_manager

---
+++ src/databank/db_manager.py
@@ -0,0 +1,42 @@
+import os
+import psycopg2
+
+""""
+Copyright © Krypton 2019-2023 - https://github.com/kkrypt0nn (https://krypton.ninja)
+Description:
+🐍 A simple template to start to code your own and personalized discord bot in Python programming language.
+
+Version: 5.5.0
+"""
+
+async def add_pr(user_id: str, exercise:str, weight:float, lifted_at=None):
+    try:
+        with psycopg2.connect(
+            host='gymbrobot_postgres', dbname='pg_gymbrobot', user=os.environ.get('POSTGRES_USER'), password=os.environ.get('POSTGRES_PASSWORD')
+        ) as con:
+            with con.cursor() as cursor:
+                cursor.execute(
+                    "INSERT INTO pr(user_id, exercise, weight, lifted_at) VALUES (%s, %s, %s, %s)",
+                    (str(user_id), str(exercise), float(weight), lifted_at)
+                )
+                con.commit()
+                return (True, None)
+            
+    except Exception as err:
+        print(err)
+        return (False, err)
+
+
+async def get_prs_from_user(user_id: str) -> list:
+    try:
+        with psycopg2.connect(
+        host=os.environ.get('POSTGRES_HOST'), dbname=os.environ.get('POSTGRES_DB'), user=os.environ.get('POSTGRES_USER'), password=os.environ.get('POSTGRES_PASSWORD')
+    ) as con:
+            with con.cursor() as cursor:
+                cursor.execute(
+                    "SELECT exercise, weight, lifted_at FROM pr WHERE user_id=%s", (user_id, )
+                )
+                return cursor.fetchall()
+
+    except Exception as err:
+        return [-1, err]
