36046fd87af6fa155ec9a99c4bb9afadb1dd4822
SDeVuyst <silasdevuyst@hotmail.com>
2024-12-21 09:49:15 +0100

    added new exercises

--- src/helpers/__init__.py
+++ src/helpers/__init__.py
@@ -1,20 +1,47 @@
 from .graph import *
 from .reps_calc import *
 
-
+# choices can only have 25 elements
 EXERCISE_CHOICES = [
     discord.app_commands.Choice(name="Bench", value="bench"),
+    discord.app_commands.Choice(name="Incline Bench", value="incline-bench"),
     discord.app_commands.Choice(name="Deadlift", value="deadlift"),
     discord.app_commands.Choice(name="Squats", value="squats"),
+    discord.app_commands.Choice(name="Push-ups", value="pushups"),
+    discord.app_commands.Choice(name="Pull-ups", value="pullups"),
+    discord.app_commands.Choice(name="Dips", value="dips"),
+    discord.app_commands.Choice(name="Dumbell Curl", value="dumbell-curl"),
+    discord.app_commands.Choice(name="Incline Dumbell Curl", value="incline-dumbell-curl"),
+    discord.app_commands.Choice(name="Barbell Curl", value="barbell-curl"),
+    discord.app_commands.Choice(name="Tricep Rope Pushdown", value="tricep-rope-pushdown"),
+    discord.app_commands.Choice(name="Dumbell Lateral Raise", value="dumbell-lateral-raise"),
+    discord.app_commands.Choice(name="Lat Pulldown", value="lat-pulldown"),
+    discord.app_commands.Choice(name="Rows", value="rows"),
+    discord.app_commands.Choice(name="Leg Extension", value="leg-extension"),
+    discord.app_commands.Choice(name="Leg Curl", value="leg-curl"),
 ]
 
 EXERCISE_IMAGES = {
     "bench": "https://cdn-0.weighttraining.guide/wp-content/uploads/2016/05/Barbell-Bench-Press-resized.png?ezimgfmt=ng%3Awebp%2Fngcb4",
+    "incline-bench": "https://cdn-0.weighttraining.guide/wp-content/uploads/2016/11/incline-barbell-bench-press-resized.png?ezimgfmt=ng%3Awebp%2Fngcb4",
     "deadlift": "https://cdn-0.weighttraining.guide/wp-content/uploads/2016/05/Barbell-Deadlift-1.png?ezimgfmt=ng%3Awebp%2Fngcb4",
     "squats": "https://cdn-0.weighttraining.guide/wp-content/uploads/2016/10/barbell-squat-resized-FIXED-2.png?ezimgfmt=ng%3Awebp%2Fngcb4",
+    "pushups": "https://cdn-0.weighttraining.guide/wp-content/uploads/2016/10/push-up-tall-resized.png?ezimgfmt=ng%3Awebp%2Fngcb4",
+    "pullups": "https://cdn-0.weighttraining.guide/wp-content/uploads/2016/10/pull-up-2-resized.png?ezimgfmt=ng%3Awebp%2Fngcb4",
+    "dips": "https://cdn-0.weighttraining.guide/wp-content/uploads/2016/05/Triceps-Dip-resized.png?ezimgfmt=ng%3Awebp%2Fngcb4",
+    "dumbell-curl": "https://cdn-0.weighttraining.guide/wp-content/uploads/2022/06/Two-arm-dumbbell-curl.png?ezimgfmt=ng%3Awebp%2Fngcb4",
+    "incline-dumbell-curl": "https://cdn-0.weighttraining.guide/wp-content/uploads/2022/02/Incline-dumbbell-inner-biceps-curl.png?ezimgfmt=ng%3Awebp%2Fngcb4",
+    "barbell-curl": "https://cdn-0.weighttraining.guide/wp-content/uploads/2016/05/barbell-curl-resized.png?ezimgfmt=ng%3Awebp%2Fngcb4",
+    "tricep-rope-pushdown": "https://cdn-0.weighttraining.guide/wp-content/uploads/2016/05/Triceps-Rope-Pushdown-resized.png?ezimgfmt=ng%3Awebp%2Fngcb4",
+    "dumbell-lateral-raise": "https://cdn-0.weighttraining.guide/wp-content/uploads/2016/05/dumbbell-lateral-raise-resized.png?ezimgfmt=ng%3Awebp%2Fngcb4",
+    "lat-pulldown": "https://cdn-0.weighttraining.guide/wp-content/uploads/2017/01/Lat-pull-down-resized.png?ezimgfmt=ng%3Awebp%2Fngcb4",
+    "rows": "https://cdn-0.weighttraining.guide/wp-content/uploads/2023/09/Straight-back-seated-cable-row-with-straight-bar.png?ezimgfmt=ng%3Awebp%2Fngcb4",
+    "leg-extension": "https://cdn-0.weighttraining.guide/wp-content/uploads/2016/05/lever-leg-extension-resized.png?ezimgfmt=ng%3Awebp%2Fngcb4",
+    "leg-curl": "https://cdn-0.weighttraining.guide/wp-content/uploads/2016/10/seated-leg-curl-resized.png?ezimgfmt=ng%3Awebp%2Fngcb4",
 }
 
 def getImageFromExercise(exercise):
+
     return EXERCISE_IMAGES[exercise]
 
 def getDiscordTimeStamp(old_timestamp):
