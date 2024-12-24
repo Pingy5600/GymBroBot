cb1a0ec90486f06904b1a53934025cdffa0e5716
SDeVuyst <silasdevuyst@hotmail.com>
2024-12-24 16:37:52 +0100

    fixed 3d graph colors

--- src/helpers/graph.py
+++ src/helpers/graph.py
@@ -10,6 +10,20 @@ from matplotlib.patches import Patch
 
 from databank import db_manager
 
+# Specifieke kleurtoewijzing voor bepaalde gebruikers-ID's
+COLOR_MAP = {
+    "464400950702899211": "#4169e1",
+    "462932133170774036": "#ff0000",
+    "559715606014984195": "#084808",
+    "733845345225670686": "#2ecc71",
+    "334371900170043402": "#fe6900",
+    "222415043550117888": "#fff200",
+    "548544519793016861": "#30D5C8",
+}
+
+FALLBACK_COLORS = ['#2a9d8f', '#e76f51', 'r', 'c', 'm', 'y', 'k']
+BACKGROUND_COLOR = '#fef9ef'
+
 async def setGraph(POOL, loop, message, users_prs, embed):
     file = await loop.run_in_executor(POOL, generate_graph, users_prs)
     embed.set_image(url="attachment://graph.gif")
@@ -26,7 +40,7 @@ async def set3DGraph(POOL, loop, message, users, exercise, embed):
             for pr in prs:
                 # Converteer naar float voor compatibiliteit
                 data.append((
-                    user.display_name,
+                    user,
                     int(pr['reps']),                          # Aantal reps als integer
                     float(pr['lifted_at'].timestamp()),       # Datum als float-timestamp
                     float(pr['weight'])                      # Gewicht als float
@@ -43,20 +57,6 @@ async def set3DGraph(POOL, loop, message, users, exercise, embed):
 
 
 def generate_graph(users_prs):
-    # Specifieke kleurtoewijzing voor bepaalde gebruikers-ID's
-    color_map = {
-        "464400950702899211": "#4169e1",
-        "462932133170774036": "#ff0000",
-        "559715606014984195": "#084808",
-        "733845345225670686": "#2ecc71",
-        "334371900170043402": "#fe6900",
-        "222415043550117888": "#fff200",
-        "548544519793016861": "#30D5C8",
-    }
-
-    # Mogelijke kleuren voor andere gebruikers
-    fallback_colors = ['#2a9d8f', '#e76f51', 'r', 'c', 'm', 'y', 'k']
-    background_color = '#fef9ef'
 
     # Structuur van de data
     user_data = []
@@ -68,8 +68,8 @@ def generate_graph(users_prs):
         weights = [pr[1] for pr in prs]
 
         # Gebruik een specifieke kleur als die beschikbaar is, anders een fallback
-        color = color_map.get(str(user.id), fallback_colors[fallback_index % len(fallback_colors)])
-        if str(user.id) not in color_map:
+        color = COLOR_MAP.get(str(user.id), FALLBACK_COLORS[fallback_index % len(FALLBACK_COLORS)])
+        if str(user.id) not in COLOR_MAP:
             fallback_index += 1
 
         user_data.append((user.display_name, dates, weights, color))
@@ -85,15 +85,15 @@ def generate_graph(users_prs):
 
     # Maak de figuur en as
     fig, ax = plt.subplots(figsize=(12, 8))
-    fig.set_facecolor(background_color)
-    ax.set_facecolor(background_color)
+    fig.set_facecolor(BACKGROUND_COLOR)
+    ax.set_facecolor(BACKGROUND_COLOR)
     ax.set_xlabel("Date")
     ax.set_ylabel("Weight (kg)")
     ax.grid(True)
 
     def update(frame):
         ax.clear()
-        ax.set_facecolor(background_color)
+        ax.set_facecolor(BACKGROUND_COLOR)
         ax.set_xlabel("Date")
         ax.set_ylabel("Weight (kg)")
         ax.grid(True)
@@ -165,31 +165,17 @@ def generate_3d_graph(data):
     if not data:
         return "No data found for the specified users and exercise."
     
-    # Specifieke kleurtoewijzing voor bepaalde gebruikers-ID's
-    color_map = {
-        "464400950702899211": "#4169e1",
-        "462932133170774036": "#ff0000",
-        "559715606014984195": "#084808",
-        "733845345225670686": "#2ecc71",
-        "334371900170043402": "#fe6900",
-        "222415043550117888": "#fff200",
-        "548544519793016861": "#30D5C8",
-    }
-    
-    fallback_colors = ['#2a9d8f', '#e76f51', 'r', 'c', 'm', 'y', 'k']
-    background_color = '#fef9ef'
-    
     # Data voorbereiden voor plotting
     fig = plt.figure(figsize=(10, 6))
     ax = fig.add_subplot(111, projection='3d')
-    ax.set_facecolor(background_color)
+    ax.set_facecolor(BACKGROUND_COLOR)
 
     legend_labels = []
     legend_handles = []
     
     fallback_index = 0  # Index voor fallback kleuren
 
-    for user_id, user_data in groupby(sorted(data, key=lambda x: x[0]), key=lambda x: x[0]):
+    for user, user_data in groupby(sorted(data, key=lambda x: x[0].display_name), key=lambda x: x[0]):
         user_data = list(user_data)  # Maak van de iterator een lijst
         if not user_data:
             continue  # Sla lege datasets over
@@ -197,17 +183,18 @@ def generate_3d_graph(data):
         user_reps = [pr[1] for pr in user_data]
         user_dates = [pr[2] for pr in user_data]
         user_weights = [pr[3] for pr in user_data]
-
+        
         # Gebruik een specifieke kleur als die beschikbaar is, anders een fallback
-        color = color_map.get(str(user_id), fallback_colors[fallback_index % len(fallback_colors)])
-        if str(user_id) not in color_map:
+        # get user id from user_data
+        color = COLOR_MAP.get(str(user.id), FALLBACK_COLORS[fallback_index % len(FALLBACK_COLORS)])
+        if str(user.id) not in COLOR_MAP:
             fallback_index += 1
 
         # Gebruik plot_trisurf om de punten te verbinden
-        ax.plot_trisurf(user_reps, user_dates, user_weights, color=color, alpha=0.8)
+        ax.plot_trisurf(user_reps, user_dates, user_weights, color=color, alpha=0.8) # alpha is transparantie
 
         # Voeg de gebruikersnaam toe aan de legenda
-        legend_labels.append(user_data[0][4] if len(user_data[0]) > 4 else f"User {user_id}")
+        legend_labels.append(user_data[0][4] if len(user_data[0]) > 4 else user.display_name)
         legend_handles.append(Patch(color=color, label=legend_labels[-1]))
 
     # Assen labels instellen
